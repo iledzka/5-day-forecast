bd8ab4f97f406da5df88074f40b310c8f016f05c
Iza Ledzka <izaledzka@gmail.com>
2019-10-10 22:58:16 +0100

    Updates

---
+++ precache-manifest.7125016e8ecb16306de5a713d4680607.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a16a2ae14977a593f9ec902240ae90c",
+    "url": "/5-day-forecast/index.html"
+  },
+  {
+    "revision": "7e33354df7f9626a5811",
+    "url": "/5-day-forecast/static/css/main.8fbab5b8.chunk.css"
+  },
+  {
+    "revision": "fc021922ff1ae158adc2",
+    "url": "/5-day-forecast/static/js/2.db075623.chunk.js"
+  },
+  {
+    "revision": "7e33354df7f9626a5811",
+    "url": "/5-day-forecast/static/js/main.044d8244.chunk.js"
+  },
+  {
+    "revision": "65dcc428f3fbeca10286",
+    "url": "/5-day-forecast/static/js/runtime-main.499698f8.js"
+  }
+]);
