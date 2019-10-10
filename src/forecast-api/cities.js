4a077b586a8f407e89743a0b17ec08ef72042f76
Iza Ledzka <izaledzka@gmail.com>
2019-10-10 16:33:19 +0100

    created list of cities

---
+++ src/forecast-api/cities.js
@@ -0,0 +1,40 @@
+const cities = () => [
+  {
+    id: 2650225,
+    name: "Edinburgh",
+    country: "GB",
+    coord: {
+      lon: -3.19648,
+      lat: 55.952061
+    }
+  },
+  {
+    id: 3094802,
+    name: "Krakow",
+    country: "PL",
+    coord: {
+      lon: 19.91667,
+      lat: 50.083328
+    }
+  },
+  {
+    id: 3096002,
+    name: "Klucze",
+    country: "PL",
+    coord: {
+      lon: 19.562361,
+      lat: 50.335651
+    }
+  },
+  {
+    id: 2648579,
+    name: "Glasgow",
+    country: "GB",
+    coord: {
+      lon: -4.25763,
+      lat: 55.86515
+    }
+  }
+];
+
+export default cities;
