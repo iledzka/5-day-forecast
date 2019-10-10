6a6e1f9f5afe37d756d02d610687509c21150402
Iza Ledzka <izaledzka@gmail.com>
2019-10-10 15:15:08 +0100

    added param to get forecast in celsjus

--- src/forecast-api/Results.js
+++ src/forecast-api/Results.js
@@ -3,7 +3,7 @@ import * as cityList from "./city.list.json";
 function createRequestForACity(cityId) {
   return () => {
     const apiKey = "7596b7d11eb133bcb7f3e994fbb7a0ba";
-    const apiRequestString = `http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&APPID=${apiKey}`;
+    const apiRequestString = `http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&units=metric&APPID=${apiKey}`;
 
     return process.env.REACT_APP_API || apiRequestString;
   };
